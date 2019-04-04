5cfa9a4d2b970556c88468aa0163f311999b9d5b
Jeremy D Adams <47381068+JrmyAdms@users.noreply.github.com>
2019-04-04 17:35:38 +0200

    Add files via upload

---
+++ SoccerLeague/src/soccer/Goal.java
@@ -0,0 +1,12 @@
+package soccer;
+
+/**
+ *
+ * @author Administrator
+ */
+public class Goal extends GameEvent {
+    
+        public String toString(){
+        return "Goal scored";
+    }
+}
---
+++ SoccerLeague/src/soccer/Possession.java
@@ -0,0 +1,13 @@
+package soccer;
+
+/**
+ *
+ * @author ksomervi
+ */
+public class Possession extends GameEvent {
+    
+    public String toString(){
+        return "Possession";
+    }
+    
+}
